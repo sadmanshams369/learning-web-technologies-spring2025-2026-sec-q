184b5384ed6782ebda3a109cbd4330bb121818fb
sadmanshams369 <sadmanshams5000@gmail.com>
2026-04-15 13:55:43 +0600

    registration page

--- labtask5.2/index.php
+++ labtask5.2/index.php
@@ -1,6 +1,28 @@
 <?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<body>
 
-<h2>Welcome</h2>
+<div style="width:500px; margin:auto; border:1px solid black; padding:10px;">
 
-<a href="register.php">Register</a><br>
-<a href="login.php">Login</a>
+<h3 align="center">Public Home</h3>
+<hr>
+
+<div align="center">
+    <a href="index.php">Home</a> |
+    <a href="login.php">Login</a> |
+    <a href="register.php">Registration</a>
+</div>
+
+<hr>
+
+<p align="center">Welcome to the system</p>
+
+<hr>
+
+<p align="center">Copyright © 2026</p>
+
+</div>
+
+</body>
+</html>
---
+++ labtask5.2/register.php
@@ -0,0 +1,99 @@
+<?php
+session_start();
+
+if(isset($_POST['submit'])){
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirm = $_POST['confirm'];
+    $gender = $_POST['gender'];
+    $dob = $_POST['dob'];
+
+    if($password == $confirm){
+
+        $_SESSION['name'] = $name;
+        $_SESSION['email'] = $email;
+        $_SESSION['user'] = $username;
+        $_SESSION['pass'] = $password;
+        $_SESSION['gender'] = $gender;
+        $_SESSION['dob'] = $dob;
+
+        echo "Registration Successful! <br>";
+        echo "<a href='login.php'>Login Now</a>";
+
+    } else {
+        echo "Password & Confirm Password not match!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+
+<div style="width:450px; margin:auto; border:1px solid black; padding:10px;">
+
+<h3 style="text-align:center;">Registration</h3>
+
+<hr>
+
+<form method="post">
+
+<table>
+
+<tr>
+    <td>Name:</td>
+    <td><input type="text" name="name"></td>
+</tr>
+
+<tr>
+    <td>Email:</td>
+    <td><input type="email" name="email"></td>
+</tr>
+
+<tr>
+    <td>Username:</td>
+    <td><input type="text" name="username"></td>
+</tr>
+
+<tr>
+    <td>Password:</td>
+    <td><input type="password" name="password"></td>
+</tr>
+
+<tr>
+    <td>Confirm Password:</td>
+    <td><input type="password" name="confirm"></td>
+</tr>
+
+<tr>
+    <td>Gender:</td>
+    <td>
+        <input type="radio" name="gender" value="Male"> Male
+        <input type="radio" name="gender" value="Female"> Female
+    </td>
+</tr>
+
+<tr>
+    <td>Date of Birth:</td>
+    <td><input type="date" name="dob"></td>
+</tr>
+
+<tr>
+    <td></td>
+    <td><button type="submit" name="submit">Register</button></td>
+</tr>
+
+</table>
+
+</form>
+
+</div>
+
+</body>
+</html>
